bb4e01b601d207f461018157fff68c564ed97075
Jones Ho <xiaoyaojones@163.com>
2014-02-20 18:49:05 +0800

    add images perload
    add images perload

--- js/god.js
+++ js/god.js
@@ -5,23 +5,53 @@ GODSSERIES = GODS.length;
 var eClick = ('ontouchstart' in window) ? 'touchstart' : 'click';
 
 var $GodBox = $('#GodBox'),
-	sHtml = '';
+	$Perload = $('#perload'),
+	sHtml = '',
+	imgSrc = [],
+	nPer = 0;
 
-for (var i = 0; i < GODSSERIES; i++) {
-	sHtml += '<div class="box">'
-	sHtml += '<strong class="name">' + GODS[i].series + '</strong><ul>'
-	$.each(GODS[i].property,function (j) {
-		if (!!this.num) {
-			sHtml += '<li><img src="images/0' + (j+1) + '/' + this.num + 'i_g.png" data-series="' + (j+1) + '" data-num="' + this.num + '" data-status="0" data-evo="' + this.evolution + '" data-u-evo="' + this.u_evolution + '" data-n-u-evo="0"></li>'
-		}else{
-			sHtml += '<li>&nbsp;</li>'
-		};
-		
-	});
-	sHtml += '</ul></div>';
+var fCreateList = function () {
+	for (var i = 0; i < GODSSERIES; i++) {
+		sHtml += '<div class="box">'
+		sHtml += '<strong class="name">' + GODS[i].series + '</strong><ul>'
+		$.each(GODS[i].property,function (j) {
+			//組合圖片列表
+			var _f = '0' + (j+1) + '/';
+			if (!!this.num) {
+				sHtml += '<li><img src="images/' + _f + this.num + 'i_g.png" data-series="' + (j+1) + '" data-num="' + this.num + '" data-status="0" data-evo="' + this.evolution + '" data-u-evo="' + this.u_evolution + '" data-n-u-evo="0"></li>'
+				imgSrc.push(_f + this.num + 'i')
+				imgSrc.push(_f + this.num + 'i_g')
+				if (!!this.evolution) {
+					imgSrc.push(_f + this.evolution + 'i')
+					if (!!this.u_evolution) {
+						for (var k = 0; k < this.u_evolution.length; k++) {
+							imgSrc.push(_f + this.u_evolution[k] + 'i')
+						};
+					};
+				};
+			}else{
+				sHtml += '<li>&nbsp;</li>'
+			};
+			
+		});
+		sHtml += '</ul></div>';
+	};
+	fPerload();
+	$GodBox.html(sHtml);
 };
 
-$GodBox.html(sHtml);
+var fPerload = function () {
+	var sPerload = '';
+
+	for (var i = 0; i < imgSrc.length; i++) {
+		sPerload += '<img src="images/' + imgSrc[i] + '.png">'
+	};
+	$Perload.html(sPerload)
+	$('body').jpreLoader(function () {
+		console.log(123)
+	})
+};
+
 
 var fTools = {
 	changeImg : function (obj,s,num) {
@@ -81,5 +111,9 @@ $btn.on(eClick,function (e) {
 	    	Canvas2Image.saveAsPNG(canvas);
 	    }
 	});
+});
+
+$(function ($) {
+	fCreateList();
 });
 
